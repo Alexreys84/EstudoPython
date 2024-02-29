85c5f5238d117405e19647b38da6f05693f9d7fc
Alex Reis <alexreis1984@gmail.com>
2024-02-29 15:29:09 -0300

    int: Iniciando o estudo em Python. Exibindo mensagem

---
+++ Aula01/boasvindas.py
@@ -0,0 +1,7 @@
+# Vamos criar ima variável para armazenar o
+# nome do usuário
+nome = "admin"
+print("Olá, "+nome+".Seja Bem Vindo")
+print("Escreva o seu Nome de usuário:")
+nome = input("Digite aqui:")
+print(O seu nome usuário é :"+nome")
