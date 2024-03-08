64d89929992b6aac471630fb21ad20a8001d3e0f
Alex Reis <alexreis1984@gmail.com>
2024-03-08 15:11:37 -0300

    modificano codig usando Funçoes

---
+++ Aula04/usar_funcoes.py
@@ -0,0 +1,9 @@
+# Import biblioteca as bb
+from biblioteca import boasvindas, anobissexto, limpa_tela, pontas_lista
+
+num = [10, 23, 85, 7, 25, 47, 0, 100]
+limpa_tela
+boasvindas()
+print(anobissexto(2024))
+print(pontas_lista(num))
+
