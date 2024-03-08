1dcefb49fe515c861e61c3afb85b379a255524c4
Alex Reis <alexreis1984@gmail.com>
2024-03-08 15:13:00 -0300

    Modificando cod com funcões

--- Aula04/usar_funcoes.py
+++ Aula04/usar_funcoes.py
@@ -2,7 +2,7 @@
 from biblioteca import boasvindas, anobissexto, limpa_tela, pontas_lista
 
 num = [10, 23, 85, 7, 25, 47, 0, 100]
-limpa_tela
+limpa_tela()
 boasvindas()
 print(anobissexto(2024))
 print(pontas_lista(num))
